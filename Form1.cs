6f8298baefaaaa19ff17fdff5ca11c3bd877320a
User <User@WIN-F21GVBTTSGJ>
2024-02-21 22:26:45 +0600

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Windows.Forms;
+
+// подключаем атрибут DllImport
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Drawing;
+using System.Globalization;
+using System.Collections.Generic;
+
+using static WindowsFormsApp1.enums;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        [DllImport("user32.dll")]
+        public static extern int MessageBox(IntPtr hWnd, String text, String caption, int options); // объявляем метод на C# 
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetComputerName(StringBuilder lpBuffer, ref int lpnSize);
+
+        [DllImport("advapi32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetUserName(StringBuilder buffer, ref uint size);
+        [DllImport("kernel32")]
+        public static extern int GetSystemDirectory(StringBuilder buf, int nMaxCount);
+       
+        private void UserName_Click(object sender, EventArgs e)
+        {
+            StringBuilder sr = new StringBuilder(255);
+            UInt32 ir = (uint)sr.Capacity;
+            GetUserName(sr, ref ir);
+            MessageBox(this.Handle,sr.ToString(), "User Name", 0);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StringBuilder sbBuf = new StringBuilder(128);
+            Int32 intLen = (int)sbBuf.Capacity;
+            GetComputerName(sbBuf, ref intLen);
+            MessageBox(this.Handle, sbBuf.ToString(), "Mashina Name", 0);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StringBuilder sSystem = new StringBuilder(255);
+            GetSystemDirectory(sSystem, sSystem.Capacity);
+            MessageBox(this.Handle, sSystem.ToString(), "Mashina Name", 0);
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        public class OSVersionInfo
+        {
+            public uint dwOSVersionInfoSize;
+            public uint dwMajorVersion;
+            public uint dwMinorVersion;
+            public uint dwBuildNumber;
+            public uint dwPlatformId;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
+            public String szCSDVersion;
+        }
+
+        class Kernel
+        {
+            [DllImport("kernel32.dll", CharSet = CharSet.Ansi)]
+            public static extern bool GetVersionEx([In, Out] OSVersionInfo info);
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OSVersionInfo version = new OSVersionInfo();
+            version.dwOSVersionInfoSize = (uint)Marshal.SizeOf(version);
+            Kernel.GetVersionEx(version);
+            MessageBox(this.Handle, version.dwBuildNumber.ToString(), "version", 0);
+        }
+        [DllImport("user32.dll")]
+        static extern int GetSystemMetrics(int nIndex);
+       
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int metr = 16;
+                    MessageBox(this.Handle, GetSystemMetrics(metr).ToString(), "Ширина и высота области приложения в полноэкранно", 0);
+             
+        }
+        [DllImport("user32.dll", EntryPoint = "SystemParametersInfoA")]
+        static extern bool SystemParametersInfo(int uAction, ref int uParam, ref int lpvParam, int fuWinIni);
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+      
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int uiParam = 0;
+            int lpParam = 0;
+            int param = 10; 
+            bool sw = SystemParametersInfo(param, ref uiParam, ref lpParam, 0);
+
+            if (sw)
+            {
+                MessageBox(this.Handle, lpParam.ToString(), "Интервал времени ожидания доступности", 0);
+            }
+            else {
+                int error = Marshal.GetLastWin32Error();
+                MessageBox(this.Handle, "Ошибка"+error.ToString(), "Интервал времени ожидания доступности", 0);
+            }
+        }
+        [DllImport("user32.dll")]
+        static extern uint GetSysColor(enums.SystemElementsToColor nIndex);
+        [DllImport("user32.dll")]
+        static extern bool SetSysColors(int cElements, int[] lpaElements, int[] lpaColors);
+        List<ItemColor> listSystemColors = new List<ItemColor>();
+        List<ItemColor> saveSystemColors()
+        {
+            List<ItemColor> list = new List<ItemColor>();//список системных цветов
+            foreach (SystemElementsToColor se in comboBox1.Items)
+            {
+                int color1 = Convert.ToInt32(GetSysColor(se));
+                Color color = Color.FromArgb(color1 & 0xFF, (color1 & 0xFF00) >> 8, (color1 & 0xFF0000) >> 16);
+                list.Add(new ItemColor(se.ToString(), color));
+
+            }
+            return list;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fillComboBoxes();//заполнение comboBox
+            listSystemColors = saveSystemColors();//сохраним начальные системные цвета
+        }
+        void fillComboBoxes()
+        {
+            comboBox1.DataSource = Enum.GetValues(typeof(WindowsFormsApp1.enums.SystemElementsToColor));
+            comboBox1.SelectedItem = null;
+            comboBox1.Text = "Выберите элемент";
+            foreach (System.Reflection.PropertyInfo prop in typeof(Color).GetProperties())
+            {
+                if (prop.PropertyType.FullName == "System.Drawing.Color")
+                {
+                    comboBox2.Items.Add(prop.Name);
+                }
+            }
+            comboBox2.SelectedItem = null;
+            comboBox2.Text = "Выберите цвет";
+        }
+
+      
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (comboBox2.SelectedItem != null && comboBox2.SelectedItem != null)
+            {
+                Color newColor = Color.FromName(comboBox2.SelectedItem.ToString());
+                SystemElementsToColor item = (SystemElementsToColor)Enum.Parse(typeof(SystemElementsToColor), comboBox1.SelectedItem.ToString());
+
+                listSystemColors.ForEach(x =>
+                {
+                    if (x.ItemName == item.ToString())
+                    {
+                        
+                        SetSysColors(1, new int[] { (int)item }, new int[] { System.Drawing.ColorTranslator.ToWin32(newColor) });
+                        x.HasChanged = true;
+                        MessageBox(this.Handle, newColor.ToString(), "COLOR_CAPTIONTEXT", 0);
+                       
+                    }
+                });
+
+            }
+            else { System.Windows.Forms.MessageBox.Show("What?"); }
+        }
+        void GetSystemColor()
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                //элемент выбранный из списка
+                SystemElementsToColor item = (SystemElementsToColor)Enum.Parse(typeof(SystemElementsToColor), comboBox1.SelectedItem.ToString());
+                //возвращает RGB цвет элемента item 
+                int color1 = Convert.ToInt32(GetSysColor(item));
+                //преобразование RGB в Color
+                Color color = Color.FromArgb(color1 & 0xFF, (color1 & 0xFF00) >> 8, (color1 & 0xFF0000) >> 16);
+                //String s = Color.FromName(color);
+                Color sd = Color.FromName(ColorTranslator.ToHtml(color));
+                
+                MessageBox(this.Handle,sd.ToString(), "COLOR_CAPTIONTEXT", 0);
+            }
+        }
+        private void button11_Click(object sender, EventArgs e)
+        {
+            listSystemColors.ForEach(x =>
+            {
+                if (x.HasChanged)
+                {
+                    Color oldColor = x.IItemColor;
+                    SystemElementsToColor item = (SystemElementsToColor)Enum.Parse(typeof(SystemElementsToColor), x.ItemName);
+                    SetSysColors(1, new int[] { (int)item }, new int[] { System.Drawing.ColorTranslator.ToWin32(oldColor) });
+                    x.HasChanged = false;
+                }
+            });
+          
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            GetSystemColor();
+        }
+        [DllImport("kernel32.dll")]
+        static extern void GetTimeZoneInformation(ref TIME_ZONE_INFORMATION lpTimeZoneInformation);
+        [DllImport("Kernel32")]
+        public static extern int GetTickCount();
+        private void button12_Click(object sender, EventArgs e)
+        {
+            int t = GetTickCount();
+            MessageBox(this.Handle, t.ToString(), "GetTickCount ", 0);
+
+        }
+
+        struct SYSTEMTIME
+        {
+            internal Int16 wYear;
+            internal Int16 wMonth;
+            internal Int16 wDayOfWeek;
+            internal Int16 wDay;
+            internal Int16 wHour;
+            internal Int16 wMinute;
+            internal Int16 wSecond;
+            internal Int16 wMilliseconds;
+        }
+
+        struct TIME_ZONE_INFORMATION
+        {
+            internal Int32 Bias;
+            internal String StandardName;
+            internal SYSTEMTIME StandardDate;
+            internal Int32 StandardBias;
+            internal String DaylightName;
+            internal SYSTEMTIME DaylightDate;
+            internal Int32 DaylightBias;
+        }
+        [DllImport("kernel32.dll")]
+        static extern void GetLocalTime(ref SYSTEMTIME lpSystemTime);
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            SYSTEMTIME syst = new SYSTEMTIME();
+            GetLocalTime(ref syst);
+           
+            switch (syst.wDayOfWeek)
+            {
+                case 1:
+                    MessageBox(this.Handle, "Понедельник", "GetTickCount ", 0);
+                    break;
+                case 2:
+                    MessageBox(this.Handle, "Вторник", "GetTickCount ", 0);
+                    break;
+                case 3:
+                    MessageBox(this.Handle, "Среда", "GetTickCount ", 0);
+                    break;
+                case 4:
+                    MessageBox(this.Handle, "Четверг", "GetTickCount ", 0);
+                    break;
+                case 5:
+                    MessageBox(this.Handle, "Пятница", "GetTickCount ", 0);
+                    break;
+                case 6:
+                    MessageBox(this.Handle, "Суббота", "GetTickCount ", 0);
+                    break;
+                case 7:
+                    MessageBox(this.Handle, "Воскресение", "GetTickCount ", 0);
+                    break;
+            }
+        }
+       
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            TIME_ZONE_INFORMATION time1 = new TIME_ZONE_INFORMATION();
+            GetTimeZoneInformation(ref time1);
+            MessageBox(this.Handle, time1.StandardName, "GetTickCount ", 0);
+        }
+        [DllImport("kernel32.dll")]
+        public static extern uint GetUserDefaultLCID();
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            uint lcid = GetUserDefaultLCID();
+            MessageBox(this.Handle,lcid.ToString(), "GetTickCount ", 0);
+
+        }
+
+        
+        [DllImport("user32.dll")]
+        public static extern int GetKeyboardLayoutList(int nBuff, [Out] IntPtr[] lpList);
+        private void button14_Click(object sender, EventArgs e)
+        {
+            int count = GetKeyboardLayoutList(0, null);
+            String s=null;
+            // Выделение памяти под массив указателей для хранения раскладок клавиатуры
+            IntPtr[] layouts = new IntPtr[count];
+            foreach (IntPtr layout in layouts)
+            {
+                int layoutInt = layout.ToInt32();
+                s = s + layoutInt.ToString()+" ";
+            }
+            label1.Text = "Available Keyboard Layouts " + s;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool ShowCursor(bool bShow);
+        private void button15_Click(object sender, EventArgs e)
+        {
+            ShowCursor(false);
+            MessageBox(this.Handle, "Курсор скрыт", "ShowCursor ", 0);
+            // Ждем 3 секунды
+            System.Threading.Thread.Sleep(3000);
+
+            // Отобразить курсор
+            ShowCursor(true);
+            MessageBox(this.Handle, "Курсор отображен", "ShowCursor ", 0);
+        }
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        public static extern bool SetLocaleInfo(uint Locale, uint LCType, string lpLCData);
+        const uint LOCALE_USER_DEFAULT = 0x0400;
+        const uint LOCALE_SSHORTDATE = 0x1F;
+        private void button16_Click(object sender, EventArgs e)
+        {
+            string newShortDateFormat = "MM/dd/yyyy";
+            bool success = SetShortDateFormat(newShortDateFormat);
+            if (success)
+            {
+                MessageBox(this.Handle, "Формат короткой даты успешно изменен", "SetLocaleInfo ", 0);
+            }
+            else
+            {
+                MessageBox(this.Handle, "Произошла ошибка при изменении формата короткой даты", " SetLocaleInfo ", 0);
+            }
+        }
+        static bool SetShortDateFormat(string dateFormat)
+        {
+            return SetLocaleInfo(LOCALE_USER_DEFAULT, LOCALE_SSHORTDATE, dateFormat);
+        }
+    }
+
+}
---
+++ enums.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class enums
+    {
+        public enum SystemMetrics
+        {
+            SM_CXSCREEN = 0,  // 0x00
+            SM_CYSCREEN = 1,  // 0x01
+            SM_CXVSCROLL = 2,  // 0x02
+            SM_CYHSCROLL = 3,  // 0x03
+            SM_CYCAPTION = 4,  // 0x04
+            SM_CXBORDER = 5,  // 0x05
+            SM_CYBORDER = 6,  // 0x06
+            SM_CXDLGFRAME = 7,  // 0x07
+            SM_CXFIXEDFRAME = 7,  // 0x07
+            SM_CYDLGFRAME = 8,  // 0x08
+            SM_CYFIXEDFRAME = 8,  // 0x08
+            SM_CYVTHUMB = 9,  // 0x09
+            SM_CXHTHUMB = 10, // 0x0A
+            SM_CXICON = 11, // 0x0B
+            SM_CYICON = 12, // 0x0C
+            SM_CXCURSOR = 13, // 0x0D
+            SM_CYCURSOR = 14, // 0x0E
+            SM_CYMENU = 15, // 0x0F
+            SM_CXFULLSCREEN = 16, // 0x10
+            SM_CYFULLSCREEN = 17, // 0x11
+            SM_CYKANJIWINDOW = 18, // 0x12
+            SM_MOUSEPRESENT = 19, // 0x13
+            SM_CYVSCROLL = 20, // 0x14
+            SM_CXHSCROLL = 21, // 0x15
+            SM_DEBUG = 22, // 0x16
+            SM_SWAPBUTTON = 23, // 0x17
+            SM_CXMIN = 28, // 0x1C
+            SM_CYMIN = 29, // 0x1D
+            SM_CXSIZE = 30, // 0x1E
+            SM_CYSIZE = 31, // 0x1F
+            SM_CXSIZEFRAME = 32, // 0x20
+            SM_CXFRAME = 32, // 0x20
+            SM_CYSIZEFRAME = 33, // 0x21
+            SM_CYFRAME = 33, // 0x21
+            SM_CXMINTRACK = 34, // 0x22
+            SM_CYMINTRACK = 35, // 0x23
+            SM_CXDOUBLECLK = 36, // 0x24
+            SM_CYDOUBLECLK = 37, // 0x25
+            SM_CXICONSPACING = 38, // 0x26
+            SM_CYICONSPACING = 39, // 0x27
+            SM_MENUDROPALIGNMENT = 40, // 0x28
+            SM_PENWINDOWS = 41, // 0x29
+            SM_DBCSENABLED = 42, // 0x2A
+            SM_CMOUSEBUTTONS = 43, // 0x2B
+            SM_SECURE = 44, // 0x2C
+            SM_CXEDGE = 45, // 0x2D
+            SM_CYEDGE = 46, // 0x2E
+            SM_CXMINSPACING = 47, // 0x2F
+            SM_CYMINSPACING = 48, // 0x30
+            SM_CXSMICON = 49, // 0x31
+            SM_CYSMICON = 50, // 0x32
+            SM_CYSMCAPTION = 51, // 0x33
+            SM_CXSMSIZE = 52, // 0x34
+            SM_CYSMSIZE = 53, // 0x35
+            SM_CXMENUSIZE = 54, // 0x36
+            SM_CYMENUSIZE = 55, // 0x37
+            SM_ARRANGE = 56, // 0x38
+            SM_CXMINIMIZED = 57, // 0x39
+            SM_CYMINIMIZED = 58, // 0x3A
+            SM_CXMAXTRACK = 59, // 0x3B
+            SM_CYMAXTRACK = 60, // 0x3C
+            SM_CXMAXIMIZED = 61, // 0x3D
+            SM_CYMAXIMIZED = 62, // 0x3E
+            SM_NETWORK = 63, // 0x3F
+            SM_CLEANBOOT = 67, // 0x43
+            SM_CXDRAG = 68, // 0x44
+            SM_CYDRAG = 69, // 0x45
+            SM_SHOWSOUNDS = 70, // 0x46
+            SM_CXMENUCHECK = 71, // 0x47
+            SM_CYMENUCHECK = 72, // 0x48
+            SM_SLOWMACHINE = 73, // 0x49
+            SM_MIDEASTENABLED = 74, // 0x4A
+            SM_MOUSEWHEELPRESENT = 75, // 0x4B
+            SM_XVIRTUALSCREEN = 76, // 0x4C
+            SM_YVIRTUALSCREEN = 77, // 0x4D
+            SM_CXVIRTUALSCREEN = 78, // 0x4E
+            SM_CYVIRTUALSCREEN = 79, // 0x4F
+            SM_CMONITORS = 80, // 0x50
+            SM_SAMEDISPLAYFORMAT = 81, // 0x51
+            SM_IMMENABLED = 82, // 0x52
+            SM_CXFOCUSBORDER = 83, // 0x53
+            SM_CYFOCUSBORDER = 84, // 0x54
+            SM_TABLETPC = 86, // 0x56
+            SM_MEDIACENTER = 87, // 0x57
+            SM_STARTER = 88, // 0x58
+            SM_SERVERR2 = 89, // 0x59
+            SM_MOUSEHORIZONTALWHEELPRESENT = 91, // 0x5B
+            SM_CXPADDEDBORDER = 92, // 0x5C
+            SM_DIGITIZER = 94, // 0x5E
+            SM_MAXIMUMTOUCHES = 95, // 0x5F
+            SM_REMOTESESSION = 0x1000, // 0x1000
+            SM_SHUTTINGDOWN = 0x2000, // 0x2000
+            SM_REMOTECONTROL = 0x2001, // 0x2001
+            SM_CONVERTIBLESLATEMODE = 0x2003,
+            SM_SYSTEMDOCKED = 0x2004,
+        }
+        public enum SystemElementsToColor
+        {
+          
+            COLOR_3DLIGHT = 22,//Light color for three-dimensional display elements(for edges facing the light source.)
+            COLOR_GRADIENTINACTIVECAPTION = 28,//Right side color in the color gradient of an inactive window's title bar. COLOR_INACTIVECAPTION specifies the left side color.
+            COLOR_MENU = 4,//Menu background. The associated foreground color is COLOR_MENUTEXT
+        }
+        /*    public enum ColorFromRGB { 
+                maroon=	"#800000,
+    dark red	#8B0000
+    brown	#A52A2A
+    firebrick	#B22222
+    crimson	#DC143C
+    red	#FF0000
+    tomato	#FF6347
+    coral	#FF7F50
+    indian red	#CD5C5C
+    light coral	#F08080
+    dark salmon	#E9967A
+    salmon	#FA8072
+    light salmon	#FFA07A
+    orange red	#FF4500
+    dark orange	#FF8C00
+    orange	#FFA500
+    gold	#FFD700
+    dark golden rod	#B8860B
+    golden rod	#DAA520
+    pale golden rod	#EEE8AA
+    dark khaki	#BDB76B
+    khaki	#F0E68C
+    olive	#808000
+    yellow	#FFFF00
+    yellow green	#9ACD32
+    dark olive green	#556B2F
+    olive drab	#6B8E23
+    lawn green	#7CFC00
+    chart reuse	#7FFF00
+    green yellow	#ADFF2F
+    dark green	#006400
+    green	#008000
+    forest green	#228B22
+    lime	#00FF00
+    lime green	#32CD32
+    light green	#90EE90
+    pale green	#98FB98
+    dark sea green	#8FBC8F
+    medium spring green	#00FA9A
+    spring green	#00FF7F
+    sea green	#2E8B57
+    medium aqua marine	#66CDAA
+    medium sea green	#3CB371
+    light sea green	#20B2AA
+    dark slate gray	#2F4F4F
+    teal	#008080
+    dark cyan	#008B8B
+    aqua	#00FFFF
+    cyan	#00FFFF
+    light cyan	#E0FFFF
+    dark turquoise	#00CED1
+    turquoise	#40E0D0
+    medium turquoise	#48D1CC
+    pale turquoise	#AFEEEE
+    aqua marine	#7FFFD4
+    powder blue	#B0E0E6
+    cadet blue	#5F9EA0
+    steel blue	#4682B4
+    corn flower blue	#6495ED
+    deep sky blue	#00BFFF
+    dodger blue	#1E90FF
+    light blue	#ADD8E6
+    sky blue	#87CEEB
+    light sky blue	#87CEFA
+    midnight blue	#191970
+    navy	#000080
+    dark blue	#00008B
+    medium blue	#0000CD
+    blue	#0000FF
+    royal blue	#4169E1
+    blue violet	#8A2BE2
+    indigo	#4B0082
+    dark slate blue	#483D8B
+    slate blue	#6A5ACD
+    medium slate blue	#7B68EE
+    medium purple	#9370DB
+    dark magenta	#8B008B
+    dark violet	#9400D3
+    dark orchid	#9932CC
+    medium orchid	#BA55D3
+    purple	#800080
+    thistle	#D8BFD8
+    plum	#DDA0DD
+    violet	#EE82EE
+    magenta / fuchsia	#FF00FF
+    orchid	#DA70D6
+    medium violet red	#C71585
+    pale violet red	#DB7093
+    deep pink	#FF1493
+    hot pink	#FF69B4
+    light pink	#FFB6C1
+    pink	#FFC0CB
+    antique white	#FAEBD7
+    beige	#F5F5DC
+    bisque	#FFE4C4
+    blanched almond	#FFEBCD
+    wheat	#F5DEB3
+    corn silk	#FFF8DC
+    lemon chiffon	#FFFACD
+    light golden rod yellow	#FAFAD2
+    light yellow	#FFFFE0
+    saddle brown	#8B4513
+    sienna	#A0522D
+    chocolate	#D2691E
+    peru	#CD853F
+    sandy brown	#F4A460
+    burly wood	#DEB887
+    tan	#D2B48C
+    rosy brown	#BC8F8F
+    moccasin	#FFE4B5
+    navajo white	#FFDEAD
+    peach puff	#FFDAB9
+    misty rose	#FFE4E1
+    lavender blush	#FFF0F5
+    linen	#FAF0E6
+    old lace	#FDF5E6
+    papaya whip	#FFEFD5
+    sea shell	#FFF5EE
+    mint cream	#F5FFFA
+    slate gray	#708090
+    light slate gray	#778899
+    light steel blue	#B0C4DE
+    lavender	#E6E6FA
+    floral white	#FFFAF0
+    alice blue	#F0F8FF
+    ghost white	#F8F8FF
+    honeydew	#F0FFF0
+    ivory	#FFFFF0
+    azure	#F0FFFF
+    snow	#FFFAFA
+    black	#000000
+    dim gray / dim grey	#696969
+    gray / grey	#808080
+    dark gray / dark grey	#A9A9A9
+    silver	#C0C0C0
+    light gray / light grey	#D3D3D3
+    gainsboro	#DCDCDC
+    white smoke	#F5F5F5
+    white	#FFFFFF
+    */
+    }
+}
